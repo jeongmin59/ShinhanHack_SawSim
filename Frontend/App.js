562cfc673b4c0cb87334d7780dd159c8bb65b4b0
이가영 <lgy4625@naver.com>
2023-09-02 16:05:33 +0900

    #2 [FEAT] : 앱 생성

---
+++ Frontend/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>리액트네이티브 너무 무서워요</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
