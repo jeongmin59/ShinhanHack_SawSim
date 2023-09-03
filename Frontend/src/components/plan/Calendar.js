bd64d4dd8cea03f6b4681f0bdb2d9c1bf3ada8fd
공정민 <jmgong59@gmail.com>
2023-09-03 12:17:01 +0900

    #5 [FEAT] : 달력 선택 모달 생성

--- Frontend/src/components/plan/Calendar.js
+++ Frontend/src/components/plan/Calendar.js
@@ -1,1 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Calendar.module.css';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from "date-fns/esm/locale";
+
+const Calendar = () => {
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+
+  return (
+    <DatePicker
+      className={styles.datepicker}  // css 적용을 위함
+      locale={ko}   // 한국어 적용
+      dateFormat='yyyy-MM-dd'   // 날짜 형태
+      
+      selectsRange={true}
+      startDate={startDate}
+      endDate={endDate}
+      onChange={(update) => {
+        setDateRange(update);
+      }}
+      withPortal
+    />
+  );
+}
+
+export default Calendar;
--- Frontend/src/pages/Plan.js
+++ Frontend/src/pages/Plan.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import styles from "./Plan.module.css";
+// import styles from "./Plan.module.css";
+import Calendar from '../components/plan/Calendar';
 
 const Plan = () => {
-    return (
-        <div>
-            <p>예산</p>
-            <div className={styles.div}></div>
-        </div>
-    )
+  return (
+      <div>
+        <p>여행 일정</p>
+        <Calendar />
+      </div>
+  )
 }
 
 export default Plan
