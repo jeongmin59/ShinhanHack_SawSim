0e4638d3bfeb7686a65731673572bc2bf0a65397
이가영 <lgy4625@naver.com>
2023-09-03 15:24:57 +0900

    #14 [FEAT] : 헤더 컴포넌트

---
+++ Frontend/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./Login.module.css";
+import Header from "../components/common/Header"
+
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>1원이체</h1>
+    </div>
+  )
+}
+
+export default Login
