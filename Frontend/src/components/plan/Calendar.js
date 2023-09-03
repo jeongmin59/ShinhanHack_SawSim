305ceb8356866d4139838a6d7a3aca465be7350e
공정민 <jmgong59@gmail.com>
2023-09-03 21:24:19 +0900

    #5 [FEAT] : 일정 null 값 팝업 창 띄우기

--- Frontend/src/components/plan/Calendar.js
+++ Frontend/src/components/plan/Calendar.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Calendar.module.css';
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css';
 import { ko } from "date-fns/esm/locale";
-import { AiFillCalendar } from 'react-icons/ai'  // react-icon 라이브러리 (달력)
+// import { AiFillCalendar } from 'react-icons/ai'  // react-icon 라이브러리 (달력)
 import { BsDash } from 'react-icons/bs';
 
 
-const Calendar = () => {
-  const [dateRange, setDateRange] = useState([null, null]);
+const Calendar = ({ dateRange, setDateRange }) => {
+  // const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
 
+  console.log('startDate:', startDate);
+  console.log('endDate:', endDate);
+  
   return (
     <div className={styles.DateBoxContainer}>
       <div className={styles.DateBox}>
@@ -25,7 +28,7 @@ const Calendar = () => {
           startDate={startDate}
           endDate={endDate}
         />
-        <AiFillCalendar className={styles.CalendarIcon} />
+        {/* <AiFillCalendar className={styles.CalendarIcon} /> */}
       </div>
       <BsDash />
       <div className={styles.DateBox}>
@@ -39,8 +42,9 @@ const Calendar = () => {
           selectsEnd
           startDate={startDate}
           endDate={endDate}
+          minDate={startDate || null}
         />
-        <AiFillCalendar className={styles.CalendarIcon} />
+        {/* <AiFillCalendar className={styles.CalendarIcon} /> */}
       </div>
     </div>
   );
