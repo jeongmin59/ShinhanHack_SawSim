15ce4a152528d5ee4933dd62be2ee47991b31f80
공정민 <jmgong59@gmail.com>
2023-09-05 23:34:08 +0900

    #46 [REFACTOR] 선택된 일정 컴포넌트 로컬스토리지로 띄움

--- Frontend/src/components/plan/Calendar.js
+++ Frontend/src/components/plan/Calendar.js
@@ -3,35 +3,62 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { Button, Modal } from 'antd';
 import "./Calendar.css"
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'; // dd일 이 아니라 d로 표시하게 함
+import axios from 'axios';
 
 const CalendarModal = () => {
   const [open, setOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
   // const [modalText, setModalText] = useState('Content of the modal');
-
   const [date, setDate] = useState(new Date());
 
+  const saveDateToLocalStorage = (selectedDate) => {
+    try {
+      // date를 문자열로 변환하여 로컬 스토리지에 저장
+      localStorage.setItem('selectedDate', JSON.stringify(selectedDate));
+    } catch (error) {
+      console.error('로컬 스토리지 저장 안 됨', error);
+    }
+  };
+  
   const showModal = () => {
     setOpen(true);
   };
 
-  const handleOk = () => {
-    // setModalText('The modal will be closed after two seconds');
+  const handleOk = async () => {
+    // setModalText('2초후 닫힘');
     setConfirmLoading(true);
+
+    try {
+      // 서버에 보낼 데이터
+      const requestData = {
+        dataBody: {
+          startDate: date[0], // 시작 날짜
+          endDate: date[1],   // 종료 날짜
+        },
+      };
+      // POST 요청
+      const response = await axios.post('/plan', requestData);
+      console.log('요청 성공:', response.data);
+      saveDateToLocalStorage(date)
+
+    } catch (error) {
+      console.error('요청 실패:', error);
+      saveDateToLocalStorage(date)
+    }
+
     setTimeout(() => {
       setOpen(false);
       setConfirmLoading(false);
-    }, 2000);
+    }, 500);
+    window.location.reload();
   };
 
   const handleCancel = () => {
     console.log('취소');
     setOpen(false);
   };
-  console.log(date)
-  // console.log('startDate:', selection.startDate);
-  // console.log('endDate:', selection.endDate);
+  // console.log(date)
 
   return (
     <div>
