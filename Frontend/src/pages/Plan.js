5305f946dc913c33da391a50830d1697b00ff7c8
공정민 <jmgong59@gmail.com>
2023-09-05 20:20:04 +0900

    #46 [REFACTOR] : 날짜별 컴포넌트 리팩토링 전 백업

--- Frontend/src/pages/Plan.js
+++ Frontend/src/pages/Plan.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from '../pages/Plan.module.css'
 import Header from '../components/common/Header'
 import TripDate from '../components/plan/TripDate';
-// import DateList from '../components/plan/DateList';
+import DateList from '../components/plan/DateList';
 
 const Plan = () => {
   return (
     <div className={styles.container}>
       <Header />
       <TripDate />
+      <DateList />
     </div>
   );
 }
