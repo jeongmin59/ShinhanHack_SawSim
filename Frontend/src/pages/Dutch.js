ffe10737e52a98a8d6bce858306014d8965dd175
gayoung <lgy4625@naver.com>
2023-09-08 04:28:48 +0900

    #61 [FEAT] : 엔빵 카카오톡 공유

--- Frontend/src/pages/Dutch.js
+++ Frontend/src/pages/Dutch.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useLocation } from "react-router-dom";
 import styles from "./Dutch.module.css";
 import Header from "../components/common/Header";
 import { Button, Table } from 'antd';
+import { ShareAltOutlined } from '@ant-design/icons'
+
+const { Kakao} = window;
+
 
 const columns = [
   {
     title: '일시',
     dataIndex: '일시',
-    width: '45%',
-
+    width: '43%',
   },
   {
     title: '상세내역',
@@ -19,25 +22,45 @@ const columns = [
     title: '금액',
     dataIndex: '금액',
     // width: '25%',/
-
   },
 ];
+
 
 
 const Dutch = () => {
   const location = useLocation();
   const selectedRows = location.state && location.state.selectedRows;
-
+  const resultUrl = window.location.href;
 
   const data = selectedRows;
+  const share = () => {
+    Kakao.Share.sendDefault({
+      objectType: 'text',
+      text: 'data',
+      link: {
+            webUrl: resultUrl,
+            mobileWebUrl: resultUrl,
+          },
+        })
+    }
+    
+  useEffect(() => {
+    Kakao.cleanup();
+    Kakao.init('dcb221d398f716119c61870ef4204001')
+    console.log(Kakao.isInitialized())
+  }, []);
+
 
   return (
     <div>
       <Header/>
+      {/* <pre>{JSON.stringify(selectedRows, null, 2)}</pre> */}
+      <div className={styles.dutchTitleNIcon}>
+        <p className={styles.dutchTitle}>정산 내역보기</p>
+        <ShareAltOutlined onClick={share} style={{fontSize: '1.4rem', paddingRight: '1.5rem'}}/>
+      </div>
       {selectedRows && (
         <div>
-          <h2>선택된 행 데이터:</h2>
-          <pre>{JSON.stringify(selectedRows, null, 2)}</pre>
           <Table
           columns={columns}
           dataSource={data}
