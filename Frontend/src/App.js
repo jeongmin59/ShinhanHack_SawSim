2dcb047c848d33b11db4b4cd0e8a211efbd88bf7
gayoung <lgy4625@naver.com>
2023-09-09 23:15:52 +0900

    #71 [FEAT] : 현금 직접 기록 및 남은 페이지들 생성

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,10 +6,12 @@ import Login from './pages/Login'
 import MainPage from './pages/MainPage'
 import Budget from './pages/Budget'
 import Cash from './pages/Cash'
+import CashCreate from './pages/CashCreate'
 import Transaction from './pages/Transaction'
 import TransactionDetail from './pages/TransactionDetail'
 import PlanBudget from './pages/PlanBudget'
 import Dutch from './pages/Dutch'
+import Popular from './pages/Popular'
 
 function App() {
   return (
@@ -20,10 +22,12 @@ function App() {
         <Route exact path='/plan' element={<Plan/>}/>
         <Route exact path='/budget' element={<Budget/>}/>
         <Route exact path='/cash' element={<Cash/>}/>
+        <Route exact path='/cash/create' element={<CashCreate/>}/>
         <Route exact path='/transaction' element={<Transaction/>}/>
         <Route exact path='/transaction/detail' element={<TransactionDetail/>}/>
         <Route exact path='/planbudget' element={<PlanBudget/>}/>
         <Route exact path='/dutch' element={<Dutch/>}/>
+        <Route exact path='/popular' element={<Popular/>}/>
       </Routes>
     </div>
   );
---
+++ Frontend/src/components/cash/CashorOCR.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from "./CashorOCR.module.css";
+import { EditOutlined, FormOutlined, ScanOutlined } from '@ant-design/icons';
+
+const CashorOCR = () => {
+  return (
+    <div>
+      <div className={styles.titles}>
+        <Link className={styles.backLink} to='/budget'><p className={styles.toBack}>&lt;</p>
+        <p className={styles.cashTitle}>현금 기록하기</p></Link> 
+      </div>
+      <div className={styles.icons}>
+        <div className={styles.iconNment}>
+          <ScanOutlined style={{marginTop: '1rem', opacity: '0.7', fontSize: '3rem'}} />
+          <p className={styles.ment}>영수증 스캔하기</p>
+        </div>
+        <Link className={styles.toCreate} to='/cash/create'>
+          <div className={styles.iconNment}>
+            <FormOutlined style={{marginTop: '1rem', opacity: '0.7', fontSize: '3rem'}} />
+            <p className={styles.ment}>직접 기록하기</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default CashorOCR
