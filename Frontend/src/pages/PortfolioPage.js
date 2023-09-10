adc0fe32fc8baa0f275b6764b2897bb3e118d8da
gayoung <lgy4625@naver.com>
2023-09-10 23:50:22 +0900

    #73 [FEAT] : 포트폴리오 페이지

---
+++ Frontend/src/pages/PortfolioPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from "./PortfolioPage.module.css";
+import Header from "../components/common/Header";
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default PortfolioPage
