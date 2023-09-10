181f3ac2f9e44ab85b0982f53f68453d9f5b74bc
gayoung <lgy4625@naver.com>
2023-09-10 23:52:47 +0900

    #73 [REFACTOR] : 코드 간결화

--- Frontend/src/pages/PlanBudget.js
+++ Frontend/src/pages/PlanBudget.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Header from "../components/common/Header";
-// import DateListDetail from "../components/planBudget/DateListDetail"
 import BudgetList from "../components/planBudget/BudgetList"
 import { Button } from 'antd';
-// import styles from "./HistoryDetail.module.css";
 
 const Planbudget = () => {
   const reloadPage = () => {
