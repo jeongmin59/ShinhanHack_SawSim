952874442c797cb7db0d6724286845a610ec9147
공정민 <jmgong59@gmail.com>
2023-09-11 20:14:29 +0900

    #69 [FEAT] : css 수정

--- Frontend/src/pages/PlanBudget.js
+++ Frontend/src/pages/PlanBudget.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import styles from '../pages/PlanBudget.module.css'
 import Header from "../components/common/Header";
 import BudgetList from "../components/planBudget/BudgetList"
-
 import { Button } from 'antd';
 import styles from './PlanBudget.module.css'
 
 const Planbudget = () => {
-  // const reloadPage = () => {
-  //   window.location.reload();
-  // }
+  const reloadPage = () => {
+    window.location.reload();
+  }
 
   return (
     <div>
      <div>
       <Header/>
-      {/* <Button onClick={reloadPage}><Link to="/plan">뒤로가기</Link></Button> */}
       <div onClick={reloadPage} className={styles.titles}>
         <Link className={styles.backLink} to='/plan'><p className={styles.toBack}>&lt;</p>
         <p className={styles.popularTitle}>뒤로가기</p></Link> 
