9712958020b95dadc5488aa07b4d4c5ca90cf1ee
gayoung <lgy4625@naver.com>
2023-09-13 07:55:48 +0900

    #81 [FEAT] : 다이어리

---
+++ Frontend/src/components/portfolio/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from "./Map.module.css";
+import Header from "../common/Header";
+
+const Map = () => {
+  return (
+    <div>
+      <p>이건지도</p>
+    </div>
+  )
+}
+
+export default Map
--- Frontend/src/pages/MainPage.js
+++ Frontend/src/pages/MainPage.js
@@ -3,6 +3,7 @@ import styles from "./MainPage.module.css";
 import Header from "../components/common/Header";
 import BalanceSchedule from "../components/main/BalanceSchedule";
 import Insurance from "../components/main/Insurance";
+import {Link} from "react-router-dom"
 
 const MainPage = () => {
   return (
@@ -10,6 +11,7 @@ const MainPage = () => {
       <Header/>
       <BalanceSchedule/>
       <Insurance/>
+      <Link to='/portfolio'>포트폴리오</Link>
     </div>
   );
 };
