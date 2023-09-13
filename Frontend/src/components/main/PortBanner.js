f2a5ad89c32efccc7c8f0b5821e1c98e846752f3
이가영 <lgy4625@naver.com>
2023-09-14 01:17:41 +0900

    [FIX] : 엄청난 빌드 에러

--- Frontend/src/components/main/PortBanner.js
+++ Frontend/src/components/main/PortBanner.js
@@ -13,8 +13,8 @@ const Insurance = () => {
   display:'flex',justifyContent:'space-around',
   borderRadius:'1.2rem', padding:'1.3rem 1rem'}}>
     <div style={{marginLeft:'0.4rem'}} >
-      <p style={{opacity:'0.8',fontSize:'0.9rem',color:'#FFF',fontFamily:'preLt',marginTop:0,marginBottom:'0.3rem',textAlign:'start'}}>세상에 하나뿐인</p>
-      <p style={{color:'white',fontSize:'1.1rem',fontFamily:'preBd',margin:'0 0 1.2rem 0',textAlign:'start'}}>나만의 여행 포트폴리오</p>
+      <p style={{opacity:'0.8',fontSize:'0.9rem',color:'#FFF',fontFamily:'preLt',marginTop:0,marginBottom:'0.3rem',textAlign:'flex-start'}}>세상에 하나뿐인</p>
+      <p style={{color:'white',fontSize:'1.1rem',fontFamily:'preBd',margin:'0 0 1.2rem 0',textAlign:'flex-start'}}>나만의 여행 포트폴리오</p>
       <Link to='/portfolio' style={{color: 'white',textDecoration:'none'}}>
         <button style={{paddingTop:'0.2rem',paddingBottom:'0.2rem',marginBottom:'0.5rem', color: 'white', border:'0.02rem solid white',borderRadius:'0.4rem',
         display:'flex',margin: 0, textDecoration:'none'}} className={styles.more}>보러가기 &gt; 
